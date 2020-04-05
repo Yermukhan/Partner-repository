187efa61e24c64ed75d5d0222a5294abdaa9494e
GoldenDrag <mr.ablaysan@gmail.com>
2020-04-05 12:44:30 +0600

    Check

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PreviewComponent } from './preview/preview.component'
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: PreviewComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PreviewComponent } from './preview/preview.component';
+import { BookDetailComponent } from './book-detail/book-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PreviewComponent,
+    BookDetailComponent
   ],
   imports: [
     BrowserModule,
